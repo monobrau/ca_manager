8b31eb61a51b7e41aac67cc23fa0f018671053ec
monobrau <52116222+monobrau@users.noreply.github.com>
2025-07-02 17:29:28 -0500

    updates

--- ca.ps1
+++ ca.ps1
@@ -348,6 +348,406 @@ function Refresh-NamedLocationsList {
     }
 }
 
+function Show-CountrySelectionDialog {
+    param(
+        [string[]]$PreselectedCountries = @()
+    )
+    
+    # Complete list of ISO 3166-1 alpha-2 country codes with names
+    $countryList = @(
+        @{Code='AD'; Name='Andorra'},
+        @{Code='AE'; Name='United Arab Emirates'},
+        @{Code='AF'; Name='Afghanistan'},
+        @{Code='AG'; Name='Antigua and Barbuda'},
+        @{Code='AI'; Name='Anguilla'},
+        @{Code='AL'; Name='Albania'},
+        @{Code='AM'; Name='Armenia'},
+        @{Code='AO'; Name='Angola'},
+        @{Code='AQ'; Name='Antarctica'},
+        @{Code='AR'; Name='Argentina'},
+        @{Code='AS'; Name='American Samoa'},
+        @{Code='AT'; Name='Austria'},
+        @{Code='AU'; Name='Australia'},
+        @{Code='AW'; Name='Aruba'},
+        @{Code='AX'; Name='Åland Islands'},
+        @{Code='AZ'; Name='Azerbaijan'},
+        @{Code='BA'; Name='Bosnia and Herzegovina'},
+        @{Code='BB'; Name='Barbados'},
+        @{Code='BD'; Name='Bangladesh'},
+        @{Code='BE'; Name='Belgium'},
+        @{Code='BF'; Name='Burkina Faso'},
+        @{Code='BG'; Name='Bulgaria'},
+        @{Code='BH'; Name='Bahrain'},
+        @{Code='BI'; Name='Burundi'},
+        @{Code='BJ'; Name='Benin'},
+        @{Code='BL'; Name='Saint Barthélemy'},
+        @{Code='BM'; Name='Bermuda'},
+        @{Code='BN'; Name='Brunei'},
+        @{Code='BO'; Name='Bolivia'},
+        @{Code='BQ'; Name='Caribbean Netherlands'},
+        @{Code='BR'; Name='Brazil'},
+        @{Code='BS'; Name='Bahamas'},
+        @{Code='BT'; Name='Bhutan'},
+        @{Code='BV'; Name='Bouvet Island'},
+        @{Code='BW'; Name='Botswana'},
+        @{Code='BY'; Name='Belarus'},
+        @{Code='BZ'; Name='Belize'},
+        @{Code='CA'; Name='Canada'},
+        @{Code='CC'; Name='Cocos (Keeling) Islands'},
+        @{Code='CD'; Name='Democratic Republic of the Congo'},
+        @{Code='CF'; Name='Central African Republic'},
+        @{Code='CG'; Name='Republic of the Congo'},
+        @{Code='CH'; Name='Switzerland'},
+        @{Code='CI'; Name='Côte d''Ivoire'},
+        @{Code='CK'; Name='Cook Islands'},
+        @{Code='CL'; Name='Chile'},
+        @{Code='CM'; Name='Cameroon'},
+        @{Code='CN'; Name='China'},
+        @{Code='CO'; Name='Colombia'},
+        @{Code='CR'; Name='Costa Rica'},
+        @{Code='CU'; Name='Cuba'},
+        @{Code='CV'; Name='Cape Verde'},
+        @{Code='CW'; Name='Curaçao'},
+        @{Code='CX'; Name='Christmas Island'},
+        @{Code='CY'; Name='Cyprus'},
+        @{Code='CZ'; Name='Czech Republic'},
+        @{Code='DE'; Name='Germany'},
+        @{Code='DJ'; Name='Djibouti'},
+        @{Code='DK'; Name='Denmark'},
+        @{Code='DM'; Name='Dominica'},
+        @{Code='DO'; Name='Dominican Republic'},
+        @{Code='DZ'; Name='Algeria'},
+        @{Code='EC'; Name='Ecuador'},
+        @{Code='EE'; Name='Estonia'},
+        @{Code='EG'; Name='Egypt'},
+        @{Code='EH'; Name='Western Sahara'},
+        @{Code='ER'; Name='Eritrea'},
+        @{Code='ES'; Name='Spain'},
+        @{Code='ET'; Name='Ethiopia'},
+        @{Code='FI'; Name='Finland'},
+        @{Code='FJ'; Name='Fiji'},
+        @{Code='FK'; Name='Falkland Islands'},
+        @{Code='FM'; Name='Micronesia'},
+        @{Code='FO'; Name='Faroe Islands'},
+        @{Code='FR'; Name='France'},
+        @{Code='GA'; Name='Gabon'},
+        @{Code='GB'; Name='United Kingdom'},
+        @{Code='GD'; Name='Grenada'},
+        @{Code='GE'; Name='Georgia'},
+        @{Code='GF'; Name='French Guiana'},
+        @{Code='GG'; Name='Guernsey'},
+        @{Code='GH'; Name='Ghana'},
+        @{Code='GI'; Name='Gibraltar'},
+        @{Code='GL'; Name='Greenland'},
+        @{Code='GM'; Name='Gambia'},
+        @{Code='GN'; Name='Guinea'},
+        @{Code='GP'; Name='Guadeloupe'},
+        @{Code='GQ'; Name='Equatorial Guinea'},
+        @{Code='GR'; Name='Greece'},
+        @{Code='GS'; Name='South Georgia and the South Sandwich Islands'},
+        @{Code='GT'; Name='Guatemala'},
+        @{Code='GU'; Name='Guam'},
+        @{Code='GW'; Name='Guinea-Bissau'},
+        @{Code='GY'; Name='Guyana'},
+        @{Code='HK'; Name='Hong Kong'},
+        @{Code='HM'; Name='Heard Island and McDonald Islands'},
+        @{Code='HN'; Name='Honduras'},
+        @{Code='HR'; Name='Croatia'},
+        @{Code='HT'; Name='Haiti'},
+        @{Code='HU'; Name='Hungary'},
+        @{Code='ID'; Name='Indonesia'},
+        @{Code='IE'; Name='Ireland'},
+        @{Code='IL'; Name='Israel'},
+        @{Code='IM'; Name='Isle of Man'},
+        @{Code='IN'; Name='India'},
+        @{Code='IO'; Name='British Indian Ocean Territory'},
+        @{Code='IQ'; Name='Iraq'},
+        @{Code='IR'; Name='Iran'},
+        @{Code='IS'; Name='Iceland'},
+        @{Code='IT'; Name='Italy'},
+        @{Code='JE'; Name='Jersey'},
+        @{Code='JM'; Name='Jamaica'},
+        @{Code='JO'; Name='Jordan'},
+        @{Code='JP'; Name='Japan'},
+        @{Code='KE'; Name='Kenya'},
+        @{Code='KG'; Name='Kyrgyzstan'},
+        @{Code='KH'; Name='Cambodia'},
+        @{Code='KI'; Name='Kiribati'},
+        @{Code='KM'; Name='Comoros'},
+        @{Code='KN'; Name='Saint Kitts and Nevis'},
+        @{Code='KP'; Name='North Korea'},
+        @{Code='KR'; Name='South Korea'},
+        @{Code='KW'; Name='Kuwait'},
+        @{Code='KY'; Name='Cayman Islands'},
+        @{Code='KZ'; Name='Kazakhstan'},
+        @{Code='LA'; Name='Laos'},
+        @{Code='LB'; Name='Lebanon'},
+        @{Code='LC'; Name='Saint Lucia'},
+        @{Code='LI'; Name='Liechtenstein'},
+        @{Code='LK'; Name='Sri Lanka'},
+        @{Code='LR'; Name='Liberia'},
+        @{Code='LS'; Name='Lesotho'},
+        @{Code='LT'; Name='Lithuania'},
+        @{Code='LU'; Name='Luxembourg'},
+        @{Code='LV'; Name='Latvia'},
+        @{Code='LY'; Name='Libya'},
+        @{Code='MA'; Name='Morocco'},
+        @{Code='MC'; Name='Monaco'},
+        @{Code='MD'; Name='Moldova'},
+        @{Code='ME'; Name='Montenegro'},
+        @{Code='MF'; Name='Saint Martin'},
+        @{Code='MG'; Name='Madagascar'},
+        @{Code='MH'; Name='Marshall Islands'},
+        @{Code='MK'; Name='North Macedonia'},
+        @{Code='ML'; Name='Mali'},
+        @{Code='MM'; Name='Myanmar'},
+        @{Code='MN'; Name='Mongolia'},
+        @{Code='MO'; Name='Macao'},
+        @{Code='MP'; Name='Northern Mariana Islands'},
+        @{Code='MQ'; Name='Martinique'},
+        @{Code='MR'; Name='Mauritania'},
+        @{Code='MS'; Name='Montserrat'},
+        @{Code='MT'; Name='Malta'},
+        @{Code='MU'; Name='Mauritius'},
+        @{Code='MV'; Name='Maldives'},
+        @{Code='MW'; Name='Malawi'},
+        @{Code='MX'; Name='Mexico'},
+        @{Code='MY'; Name='Malaysia'},
+        @{Code='MZ'; Name='Mozambique'},
+        @{Code='NA'; Name='Namibia'},
+        @{Code='NC'; Name='New Caledonia'},
+        @{Code='NE'; Name='Niger'},
+        @{Code='NF'; Name='Norfolk Island'},
+        @{Code='NG'; Name='Nigeria'},
+        @{Code='NI'; Name='Nicaragua'},
+        @{Code='NL'; Name='Netherlands'},
+        @{Code='NO'; Name='Norway'},
+        @{Code='NP'; Name='Nepal'},
+        @{Code='NR'; Name='Nauru'},
+        @{Code='NU'; Name='Niue'},
+        @{Code='NZ'; Name='New Zealand'},
+        @{Code='OM'; Name='Oman'},
+        @{Code='PA'; Name='Panama'},
+        @{Code='PE'; Name='Peru'},
+        @{Code='PF'; Name='French Polynesia'},
+        @{Code='PG'; Name='Papua New Guinea'},
+        @{Code='PH'; Name='Philippines'},
+        @{Code='PK'; Name='Pakistan'},
+        @{Code='PL'; Name='Poland'},
+        @{Code='PM'; Name='Saint Pierre and Miquelon'},
+        @{Code='PN'; Name='Pitcairn Islands'},
+        @{Code='PR'; Name='Puerto Rico'},
+        @{Code='PS'; Name='Palestine'},
+        @{Code='PT'; Name='Portugal'},
+        @{Code='PW'; Name='Palau'},
+        @{Code='PY'; Name='Paraguay'},
+        @{Code='QA'; Name='Qatar'},
+        @{Code='RE'; Name='Réunion'},
+        @{Code='RO'; Name='Romania'},
+        @{Code='RS'; Name='Serbia'},
+        @{Code='RU'; Name='Russia'},
+        @{Code='RW'; Name='Rwanda'},
+        @{Code='SA'; Name='Saudi Arabia'},
+        @{Code='SB'; Name='Solomon Islands'},
+        @{Code='SC'; Name='Seychelles'},
+        @{Code='SD'; Name='Sudan'},
+        @{Code='SE'; Name='Sweden'},
+        @{Code='SG'; Name='Singapore'},
+        @{Code='SH'; Name='Saint Helena'},
+        @{Code='SI'; Name='Slovenia'},
+        @{Code='SJ'; Name='Svalbard and Jan Mayen'},
+        @{Code='SK'; Name='Slovakia'},
+        @{Code='SL'; Name='Sierra Leone'},
+        @{Code='SM'; Name='San Marino'},
+        @{Code='SN'; Name='Senegal'},
+        @{Code='SO'; Name='Somalia'},
+        @{Code='SR'; Name='Suriname'},
+        @{Code='SS'; Name='South Sudan'},
+        @{Code='ST'; Name='São Tomé and Príncipe'},
+        @{Code='SV'; Name='El Salvador'},
+        @{Code='SX'; Name='Sint Maarten'},
+        @{Code='SY'; Name='Syria'},
+        @{Code='SZ'; Name='Eswatini'},
+        @{Code='TC'; Name='Turks and Caicos Islands'},
+        @{Code='TD'; Name='Chad'},
+        @{Code='TF'; Name='French Southern Territories'},
+        @{Code='TG'; Name='Togo'},
+        @{Code='TH'; Name='Thailand'},
+        @{Code='TJ'; Name='Tajikistan'},
+        @{Code='TK'; Name='Tokelau'},
+        @{Code='TL'; Name='Timor-Leste'},
+        @{Code='TM'; Name='Turkmenistan'},
+        @{Code='TN'; Name='Tunisia'},
+        @{Code='TO'; Name='Tonga'},
+        @{Code='TR'; Name='Turkey'},
+        @{Code='TT'; Name='Trinidad and Tobago'},
+        @{Code='TV'; Name='Tuvalu'},
+        @{Code='TW'; Name='Taiwan'},
+        @{Code='TZ'; Name='Tanzania'},
+        @{Code='UA'; Name='Ukraine'},
+        @{Code='UG'; Name='Uganda'},
+        @{Code='UM'; Name='United States Minor Outlying Islands'},
+        @{Code='US'; Name='United States'},
+        @{Code='UY'; Name='Uruguay'},
+        @{Code='UZ'; Name='Uzbekistan'},
+        @{Code='VA'; Name='Vatican City'},
+        @{Code='VC'; Name='Saint Vincent and the Grenadines'},
+        @{Code='VE'; Name='Venezuela'},
+        @{Code='VG'; Name='British Virgin Islands'},
+        @{Code='VI'; Name='United States Virgin Islands'},
+        @{Code='VN'; Name='Vietnam'},
+        @{Code='VU'; Name='Vanuatu'},
+        @{Code='WF'; Name='Wallis and Futuna'},
+        @{Code='WS'; Name='Samoa'},
+        @{Code='YE'; Name='Yemen'},
+        @{Code='YT'; Name='Mayotte'},
+        @{Code='ZA'; Name='South Africa'},
+        @{Code='ZM'; Name='Zambia'},
+        @{Code='ZW'; Name='Zimbabwe'}
+    )
+    
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = "Select Countries"
+    $form.Size = New-Object System.Drawing.Size(600, 500)
+    $form.StartPosition = "CenterParent"
+    $form.FormBorderStyle = "Sizable"
+
+    # Search box
+    $searchLabel = New-Object System.Windows.Forms.Label
+    $searchLabel.Text = "Search countries:"
+    $searchLabel.Location = New-Object System.Drawing.Point(10, 15)
+    $searchLabel.Size = New-Object System.Drawing.Size(120, 20)
+    $form.Controls.Add($searchLabel)
+
+    $searchBox = New-Object System.Windows.Forms.TextBox
+    $searchBox.Location = New-Object System.Drawing.Point(130, 12)
+    $searchBox.Size = New-Object System.Drawing.Size(200, 20)
+    $form.Controls.Add($searchBox)
+
+    # Selection info
+    $selectionLabel = New-Object System.Windows.Forms.Label
+    $selectionLabel.Text = "0 countries selected"
+    $selectionLabel.Location = New-Object System.Drawing.Point(350, 15)
+    $selectionLabel.Size = New-Object System.Drawing.Size(200, 20)
+    $selectionLabel.ForeColor = [System.Drawing.Color]::Blue
+    $form.Controls.Add($selectionLabel)
+
+    # Quick select buttons
+    $selectAllButton = New-Object System.Windows.Forms.Button
+    $selectAllButton.Text = "Select All"
+    $selectAllButton.Location = New-Object System.Drawing.Point(10, 45)
+    $selectAllButton.Size = New-Object System.Drawing.Size(80, 25)
+    $form.Controls.Add($selectAllButton)
+
+    $selectNoneButton = New-Object System.Windows.Forms.Button
+    $selectNoneButton.Text = "Select None"
+    $selectNoneButton.Location = New-Object System.Drawing.Point(100, 45)
+    $selectNoneButton.Size = New-Object System.Drawing.Size(80, 25)
+    $form.Controls.Add($selectNoneButton)
+
+    # Countries list with checkboxes
+    $countriesListBox = New-Object System.Windows.Forms.CheckedListBox
+    $countriesListBox.Location = New-Object System.Drawing.Point(10, 80)
+    $countriesListBox.Size = New-Object System.Drawing.Size(560, 320)
+    $countriesListBox.CheckOnClick = $true
+    $form.Controls.Add($countriesListBox)
+
+    # Populate the list
+    function Refresh-CountryList {
+        param([string]$SearchTerm = "")
+        
+        $countriesListBox.Items.Clear()
+        $filteredCountries = $countryList
+        
+        if (-not [string]::IsNullOrWhiteSpace($SearchTerm)) {
+            $filteredCountries = $countryList | Where-Object { 
+                $_.Name -like "*$SearchTerm*" -or $_.Code -like "*$SearchTerm*" 
+            }
+        }
+        
+        foreach ($country in $filteredCountries | Sort-Object Name) {
+            $displayText = "$($country.Code) - $($country.Name)"
+            $index = $countriesListBox.Items.Add($displayText)
+            
+            # Check if this country was preselected
+            if ($PreselectedCountries -contains $country.Code) {
+                $countriesListBox.SetItemChecked($index, $true)
+            }
+        }
+        
+        Update-SelectionCount
+    }
+
+    function Update-SelectionCount {
+        $selectedCount = $countriesListBox.CheckedItems.Count
+        $selectionLabel.Text = "$selectedCount countries selected"
+    }
+
+    # Event handlers
+    $searchBox.Add_TextChanged({
+        Refresh-CountryList -SearchTerm $searchBox.Text
+    })
+
+    $selectAllButton.Add_Click({
+        for ($i = 0; $i -lt $countriesListBox.Items.Count; $i++) {
+            $countriesListBox.SetItemChecked($i, $true)
+        }
+        Update-SelectionCount
+    })
+
+    $selectNoneButton.Add_Click({
+        for ($i = 0; $i -lt $countriesListBox.Items.Count; $i++) {
+            $countriesListBox.SetItemChecked($i, $false)
+        }
+        Update-SelectionCount
+    })
+
+    $countriesListBox.Add_ItemCheck({
+        # Use a timer to update count after the check state changes
+        $timer = New-Object System.Windows.Forms.Timer
+        $timer.Interval = 10
+        $timer.Add_Tick({
+            Update-SelectionCount
+            $timer.Stop()
+            $timer.Dispose()
+        })
+        $timer.Start()
+    })
+
+    # OK and Cancel buttons
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Text = "OK"
+    $okButton.Location = New-Object System.Drawing.Point(420, 420)
+    $okButton.Size = New-Object System.Drawing.Size(75, 30)
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.Controls.Add($okButton)
+
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Text = "Cancel"
+    $cancelButton.Location = New-Object System.Drawing.Point(505, 420)
+    $cancelButton.Size = New-Object System.Drawing.Size(75, 30)
+    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $form.Controls.Add($cancelButton)
+
+    # Initialize the list
+    Refresh-CountryList
+
+    # Show dialog and return selected countries
+    $result = $form.ShowDialog()
+    
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+        $selectedCountries = @()
+        foreach ($item in $countriesListBox.CheckedItems) {
+            # Extract country code (first 2 characters before the hyphen)
+            $countryCode = $item.ToString().Substring(0, 2)
+            $selectedCountries += $countryCode
+        }
+        return $selectedCountries
+    } else {
+        return $null
+    }
+}
+
 function Show-CountryLocationDialog {
     param(
         $listView,
@@ -1930,7 +2330,7 @@ try {
     Write-Host "  - Rename policies" -ForegroundColor White
     Write-Host "  - Copy policies (created as DISABLED for safety)" -ForegroundColor White
     Write-Host "  - Delete policies (with confirmation warnings)" -ForegroundColor White
-    Write-Host "  - Named Location management" -ForegroundColor White
+    Write-Host "  - Named Location management with checkbox country selector" -ForegroundColor White
     Write-Host "" -ForegroundColor White
     $mainForm = Create-MainForm
     Write-Host "Showing form..." -ForegroundColor Green
